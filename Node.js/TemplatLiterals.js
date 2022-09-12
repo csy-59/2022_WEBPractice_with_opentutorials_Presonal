2fda55c9d21c5647691004d1bcabf988d8aafed6
csy-59 <csy010509@naver.com>
2022-09-13 01:30:20 +0900

    [Feat] Node.js 중반까지 수업 자료 올림

---
+++ Node.js/TemplatLiterals.js
@@ -0,0 +1,3 @@
+const name = "최서연"
+const letter = `나는 바보 멍청이다. 집에가고 싶다. 나는 자고 싶다. 왜 코딩을 해야하는 걸까 흐어엉.. 나는 ${name}이다.`
+console.log(letter);
---
+++ 웹사이트 만들기/main.js
@@ -0,0 +1,72 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+function basicBodyTmeplateHTML(title, data){
+    return `<h2>${title}</h2>
+    <p>
+        ${data}
+    </p>`
+}
+
+function templateHTML(title, list, body) {
+    return `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${body}
+        </body>
+    </html>
+    `
+}
+
+function makeFileList(fileList) {
+    let list = '<ul>\n';
+    fileList.forEach(filename => {
+        list += `<li><a href="/?id=${filename}">${filename}</a></li>`
+    })
+    list += '</ul>';
+    return list;
+}
+
+const app = http.createServer(function(request,response){
+    let _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+    
+    if(pathname === '/') {
+        let title = queryData.id;
+        let data = 'Empty.';
+        
+        let list;
+        fs.readdir('./data', 'utf8', (err, fileList) => {
+            list = makeFileList(fileList);
+        })
+
+        if(queryData.id === undefined)
+        {
+            title = 'Welcome';
+            data = 'Hello, Node.js';
+            const pageTemplate = templateHTML(title, list, basicBodyTmeplateHTML(title, data));
+            response.writeHead(200);
+            response.end(pageTemplate);
+        } else {
+            fs.readFile(`./data/${title}`, 'utf8', (err, data) => {
+                const pageTemplate = templateHTML(title, list, basicBodyTmeplateHTML(title, data));
+                response.writeHead(200);
+                response.end(pageTemplate);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
+
+});
+app.listen(3000);
